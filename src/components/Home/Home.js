47d2cc5409da7a458d63dcaedd75d0cfd51a2496
khang20112002 <khangdcse161156@fpt.edu.vn>
2023-11-09 10:35:49 +0700

    fix Home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,15 +17,30 @@ export default function Home() {
     <Navigation />
     <Container className="container" maxWidth="xl" style={{padding: '10px', marginBottom: '160px'}}>
     <div style={{display:"flex", flexDirection:"column"}}>
-    <div style={{display:"grid",gridTemplateColumns:"2fr 1fr", backgroundColor:"green", height:"300px"}}>
-      <div style={{display:"flex",alignItems:"center", justifyContent:"center", flexDirection:"column"}}> <div>Header</div>
-      <div>Header 2</div>
-      <div>Detail</div>
+    <div style={{display:"grid",gridTemplateColumns:"2fr 2fr", backgroundColor:"#6DABB4", height:"700px"}}>
+      <div style={{display:"flex",
+      alignItems:"center", justifyContent:"center",     
+      flexDirection:"column"}}> 
+      <div style={{marginBottom:'10px' ,fontSize:'50px', fontWeight:'750', color:'white'}}>
+      Birds are always friends,</div>
+      <div style={{marginBottom:'50px', fontSize:'30px', fontWeight:'750', color:'#89CED8'}}
+      >Dedication is always a priority</div>
+      <div style={{color:'white'}}
+      >Boundless love for animals.
+            <br/>
+            Dedicated service.
+            <br/>
+            Customers feel secure. </div>
 </div>
-<div>HInhg</div>
+<div style={{marginTop:'80px'}}>
+  <img 
+  style={{width:'450px', height:'600px'}}
+  // src='../public/assets/img/Multicolored_Bird_PNG_Clipart-501.png'/>
+  src='https://pics.clipartpng.com/Bird_PNG_Clip_Art-1566.png'/>
+</div>
      
     </div>
-    <img src="https://cdn.animaapp.com/projects/6543ac513678c9141fbb9a38/releases/654bf6a3a8c5bd963a266510/img/frame-1.png"/>
+    <img src="https://cdn.animaapp.com/projects/6543ac513678c9141fbb9a38/releases/654bf915a58c25517864f48e/img/frame-1.png"/>
     </div>
     </Container>
 
